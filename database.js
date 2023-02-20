5840acb39ff966b13a8de45ebd057d12c30cbae2
weldxit <113974251+weldxit@users.noreply.github.com>
2023-02-21 01:51:52 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,10 @@
+const { Pool} = require('pg')
+const connectionString = 'postgres://subha:yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ@dpg-cd557gaen0hugpkke9bg-a.oregon-postgres.render.com/iot'
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+})
+
+module.exports=pool
