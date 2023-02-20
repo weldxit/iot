3f7f071d25d5f49d9e9500cdd8785462d4ab92ba
weldxit <113974251+weldxit@users.noreply.github.com>
2023-02-21 01:49:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const { json } = require('express')
+const express = require('express')
+const db =require('./queries')
+
+const app = express()
+app.use(json())
+
+app.listen(4000,()=>{
+console.log("i am running")
+})
+app.get('/getdata',db. getSensordata)
+app.post('/sensor_data',db.postSensordata)
