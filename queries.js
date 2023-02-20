b76567e26a1b7f83881edf9b4b2d1a424e8a292d
weldxit <113974251+weldxit@users.noreply.github.com>
2023-02-21 01:50:12 +0530

    Add files via upload

---
+++ queries.js
@@ -0,0 +1,21 @@
+const pool = require('./database')
+
+const getSensordata=(req,res)=>{
+pool.query('select * from sensordata',(err,result)=>{
+    if (err){
+        throw err;
+    }
+    res.status(200).send(result.rows)
+})
+
+}
+
+const postSensordata = (req,res)=>{
+    pool.query(`insert into sensordata values (${req.body.value})`,(err,result)=>{
+        if(err){
+            throw err;
+        }
+        res.status(201).send(result.rows)
+    })
+}
+module.exports ={getSensordata,postSensordata}
